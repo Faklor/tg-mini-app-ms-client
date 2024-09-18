2afa5d3fa8fa3fa997846d172fd78f37d38c538f
Faklor <faklor_br_ar@mail.ru>
2024-09-18 15:09:48 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -5,17 +5,17 @@ import axios from "axios"
 
 export default function Home() {
 
-  const [data, setData] = useState('')
+  const [data, setData] = useState('hello')
 
-  async function order(){
-    return await axios.get('http://localhost:5000/api/teches')
-  }
-  useEffect(()=>{
-    order()
-    .then(res=>{
-      setData(res.data)
-    })
-  })
+  // async function order(){
+  //   return await axios.get('http://localhost:5000/api/teches')
+  // }
+  // useEffect(()=>{
+  //   order()
+  //   .then(res=>{
+  //     setData(res.data)
+  //   })
+  // })
   
 
   return (
