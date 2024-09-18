7a7f13a82a4167ed67f1095b5b57914906da767d
Faklor <faklor_br_ar@mail.ru>
2024-09-18 14:59:07 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -6,24 +6,24 @@ import axios from "axios"
 
 export default function Home() {
 
-  const [data, setData] = useState('hello')
+  const [data, setData] = useState('')
 
-  // async function order(){
-  //   return await axios.get('http://localhost:5000/api/teches')
-  // }
-  // useEffect(()=>{
-  //   order()
-  //   .then(res=>{
-  //     setData(res.data)
-  //   })
-  // })
+  async function order(){
+    return await axios.get('http://localhost:5000/api/teches')
+  }
+  useEffect(()=>{
+    order()
+    .then(res=>{
+      setData(res.data)
+    })
+  })
   
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         
-        <h1>{data}</h1>
+        <h1 style={{color:'red'}}>{data}</h1>
       </main>
       <footer className={styles.footer}>
         
