32deb1ab8829d407e191c5575c7efbbcf9495872
Faklor <faklor_br_ar@mail.ru>
2024-09-19 09:40:32 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,24 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import axios from "axios"
-import WebApp from 'telegram-mini-app'
+//import WebApp from 'telegram-mini-app'
+import { TelegramWebAppContainer } from '@telegram-web-app/core'
 
 export default function Home() {
 
   const [data, setData] = useState('hello')
-
-  
+  let telegram = null
+   
+  useEffect(()=>{
+    
+    // if (typeof window !== "undefined") {
+    //   // Client-side-only code
+    //   // console.log(WebApp.initData)
+    // }
+    telegram = new TelegramWebAppContainer()    
+    
+    
+  },[telegram])
   
 
   return (
@@ -16,8 +27,7 @@ export default function Home() {
       <main>
         
         <h1 style={{color:'red'}}>{data}</h1>
-        <button onClick={()=>setData(WebApp.initData)}>init</button>
-        <button onClick={()=>setData(WebApp.sendData('asf'))}>send</button>
+        <button onClick={()=>telegram.WebApp.sendData('fuck')}>send</button>
       </main>
       <footer>
         
