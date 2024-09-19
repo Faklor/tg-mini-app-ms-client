3c6726e7cd9fcada61d1175834130fd48b00eacf
Faklor <faklor_br_ar@mail.ru>
2024-09-19 13:57:00 +0300

    design main page not tg-styles

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,14 @@ import './scss/mainPage.scss'
 //import WebApp from 'telegram-mini-app'
 import { TelegramWebAppContainer } from '@telegram-web-app/core'
 
+
+
 export default function Home() {
 
   const [data, setData] = useState('hello')
   let telegram = null
+
+  const navMenu = [{en:'task',ru:'Задачи'},{en:'tech',ru:'Техника'},{en:'warehouse',ru:'Склад'}]
    
   useEffect(()=>{
     
@@ -26,10 +30,16 @@ export default function Home() {
   return (
     <div>
       <main>
-        
-        <h1 style={{color:'red'}}>{data}</h1>
+        {navMenu.map((nav,index)=>{
+          return <div key={index} onClick={()=>telegram.WebApp.sendData(nav.en)}>
+            <Image src={require(`@/res/${nav.en}.svg`)} width={68} height={68} alt={nav.en}/>
+            <p>{nav.ru}</p>
+          </div>
+        })}
+        {/* <h1 style={{color:'red'}}>{data}</h1>
         <button onClick={()=>telegram.WebApp.sendData('fuck')}>send</button>
-        <button onClick={()=>setData(telegram.WebApp.initData)}>init</button>
+        <button onClick={()=>setData(telegram.WebApp.initData)}>init</button> */}
+        
       </main>
       <footer>
         
